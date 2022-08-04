861e9682ebb9880a94170f7f9f5e3846debe1b41
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 22:59:01 +0530

    Add files via upload

---
+++ Student_qtwo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Student_qtwo {
+    
+    int  rollsam=11;
+    int rolljohn=12;
+    long phonenosam=9303832517L;
+    long phonenojohn=540917206L;
+    String adrrsam="vasbvdm";
+    String addrjohn="fnjcnerm";
+   public static void main(String[] args)
+    {
+       Student_qtwo sam=new Student_qtwo();
+       Student_qtwo john=new Student_qtwo();
+       System.out.println( sam.rollsam);
+       System.out.println(john.rolljohn);
+       System.out.println(sam.phonenosam);
+       System.out.println(john.phonenojohn);
+       System.out.println(sam.adrrsam);
+       System.out.println(john.addrjohn);
+    }
+}
+
+
