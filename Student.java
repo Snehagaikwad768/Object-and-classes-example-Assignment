b5efe2f93f97e058d76129369d96d51c7a027e45
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 22:57:04 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.*;
+ class Student {
+
+   int rollno=2;
+      String name="John";
+      public static void main(String[] args)
+       {
+     
+              Student stud1=new Student();
+             System.out.println(stud1.name);
+             System.out.println(stud1.rollno);
+         }
+}
+
+
+
